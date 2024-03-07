0cd3b8e3518dcadb701d18e9e6c1f55cf6f9dbc0
Chinmay <chinz.sandur07@gmail.com>
2024-03-07 12:27:35 +0530

    add form example

--- textfield_component/src/App.js
+++ textfield_component/src/App.js
@@ -1,12 +1,47 @@
 import { TextField } from '@mui/material';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  }
+
   return (
     <div className="App">
-      <TextField type={'text'} sx={{ margin: 3 }} placeholder='Enter your name' variant='outlined' />
-      <TextField type={'email'} sx={{ margin: 3 }} placeholder='Enter your Email' variant='outlined' />
-      <TextField type={'password'} sx={{ margin: 3 }} placeholder='Enter your Password' variant='outlined' />
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="name"
+          onChange={handleChange}
+          value={inputs.name} type={'text'} sx={{ margin: 3 }} placeholder='Enter your name' variant='outlined' />
+
+        <TextField
+          name='email'
+          value={inputs.email}
+          type={'email'} sx={{ margin: 3 }} placeholder='Enter your Email' variant='outlined' />
+
+        <TextField
+          name="password"
+          value={inputs.password}
+          type={'password'} sx={{ margin: 3 }} placeholder='Enter your Password' variant='outlined' />
+
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
