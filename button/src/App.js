b33355694e613341e09d4850ab12cb5af1653ea4
Chinmay <chinz.sandur07@gmail.com>
2024-03-07 09:41:49 +0530

    add typography example

---
+++ button/src/App.js
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant='h1'>Hello</Typography>
+    </div>
+  );
+}
+
+export default App;
