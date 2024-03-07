6c9ba7872a9260d91bbf35f5a892f139a6aaf4a2
Chinmay <chinz.sandur07@gmail.com>
2024-03-07 11:51:07 +0530

    add textField example

--- textfield_component/src/App.js
+++ textfield_component/src/App.js
@@ -1,9 +1,12 @@
+import { TextField } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <TextField type={'text'} sx={{ margin: 3 }} placeholder='Enter your name' variant='outlined' />
+      <TextField type={'email'} sx={{ margin: 3 }} placeholder='Enter your Email' variant='outlined' />
+      <TextField type={'password'} sx={{ margin: 3 }} placeholder='Enter your Password' variant='outlined' />
     </div>
   );
 }
