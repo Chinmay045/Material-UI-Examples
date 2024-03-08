9873d6c1576fdd8f7917d26dc29fe074b056c721
Chinmay <chinz.sandur07@gmail.com>
2024-03-08 10:06:22 +0530

    add example for autocomplete

---
+++ autocomplete/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import { Autocomplete, Box, TextField } from '@mui/material';
+
+function App() {
+  let states = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttarakhand",
+    "Uttar Pradesh",
+    "West Bengal",
+    "Andaman and Nicobar Islands",
+    "Chandigarh",
+    "Dadra and Nagar Haveli",
+    "Daman and Diu",
+    "Delhi",
+    "Lakshadweep",
+    "Puducherry"
+  ]
+
+  return (
+    <div>
+      <Box>
+        <Autocomplete
+          sx={{ width: 300 }}
+          options={states}
+          renderInput={(params) => (<TextField {...params} label="list of states" />)}
+        />
+      </Box>
+    </div>
+  );
+}
+
+export default App;
